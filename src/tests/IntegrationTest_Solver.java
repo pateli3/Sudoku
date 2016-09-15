67c43a49b25e4b0c15fe9e355fcf7de726ce2ff5
Ian Patel <ian.patel@yahoo.com>
2016-09-15 17:27:43 -0400

    Removed a test to do in class

--- src/tests/IntegrationTest_Solver.java
+++ src/tests/IntegrationTest_Solver.java
@@ -70,6 +70,31 @@ public class IntegrationTest_Solver {
 			}
 		}
 	}
+
+	@Test
+	public void testReset() {
+		Solver board = new Solver("false");
+		
+		board.setCell(new Coord(0, 0), 1);
+		board.setCell(new Coord(1, 1), 2);
+		board.setCell(new Coord(2, 2), 3);
+		board.setCell(new Coord(3, 3), 4);
+		board.setCell(new Coord(4, 4), 5);
+		board.setCell(new Coord(5, 5), 6);
+		board.setCell(new Coord(6, 6), 7);
+		board.setCell(new Coord(7, 7), 8);
+		board.setCell(new Coord(8, 8), 9);
+		
+		board.reset();
+		
+		Cell[][] table = board.getTable();
+		for (int i = 0; i <= 8; i++) {
+			for (int j = 0; j <= 8; j++) {
+				assertFalse(table[i][j].getSet());
+				assertEquals(0, table[i][j].list().size());
+			}
+		}
+	}
 	
 	@Test
 	public void testGraphic() {
@@ -100,66 +125,6 @@ public class IntegrationTest_Solver {
 		
 		// VISUAL INSPECTION
 		// The diagonal should contain numbers 1-9 and hints should be shown
-		try {
-			TimeUnit.SECONDS.sleep(5);
-		} catch (InterruptedException e) {
-			assert(false);
-		}
-		assert(true);
-	}
-	
-	@Test
-	public void testReset() {
-		Solver board = new Solver("false");
-		
-		board.setCell(new Coord(0, 0), 1);
-		board.setCell(new Coord(1, 1), 2);
-		board.setCell(new Coord(2, 2), 3);
-		board.setCell(new Coord(3, 3), 4);
-		board.setCell(new Coord(4, 4), 5);
-		board.setCell(new Coord(5, 5), 6);
-		board.setCell(new Coord(6, 6), 7);
-		board.setCell(new Coord(7, 7), 8);
-		board.setCell(new Coord(8, 8), 9);
-		
-		board.reset();
-		
-		Cell[][] table = board.getTable();
-		for (int i = 0; i <= 8; i++) {
-			for (int j = 0; j <= 8; j++) {
-				assertFalse(table[i][j].getSet());
-				assertEquals(0, table[i][j].list().size());
-			}
-		}
-	}
-	
-	@Test
-	public void testSolve() {
-		Solver board = new Solver("false");
-		
-		board.setCell(new Coord(0, 0), 1);
-		board.setCell(new Coord(1, 1), 2);
-		board.setCell(new Coord(2, 2), 3);
-		board.setCell(new Coord(3, 3), 4);
-		board.setCell(new Coord(4, 4), 5);
-		board.setCell(new Coord(5, 5), 6);
-		board.setCell(new Coord(6, 6), 7);
-		board.setCell(new Coord(7, 7), 8);
-		board.setCell(new Coord(8, 8), 9);
-		
-		board.solve();
-		
-		Cell[][] table = board.getTable();
-		for (int i = 0; i <= 8; i++) {
-			for (int j = 0; j <= 8; j++) {
-				assertTrue(table[i][j].getSet());
-				assertEquals(1, table[i][j].list().size());
-			}
-		}
-		
-		board.repainting();
-		// VISUAL INSPECTION
-		// The diagonal should contain numbers 1-9, rest of the board should be solved
 		try {
 			TimeUnit.SECONDS.sleep(5);
 		} catch (InterruptedException e) {
